c76d9e2ca36d4ae71f5eb457678cd45b942588b4
eduardopupoo <160680483+eduardopupoo@users.noreply.github.com>
2025-03-04 12:55:05 -0300

    Adicionado dataFrame para visualizar com maior clareza os dados do câmbio

--- scraperMoeda.py
+++ scraperMoeda.py
@@ -1,5 +1,5 @@
 import requests
-import pandas
+import pandas as pd
 
 class Main():
 
@@ -11,16 +11,29 @@ class Main():
         api_key = '3465a40fa027044d680948a265576559'
         url = f"https://api.exchangeratesapi.io/latest?base=EUR&symbols=USD,GBP,JPY,BRL&access_key={api_key}"
 
-
         response = requests.get(url)
-
         if response.status_code == 200:
             data = response.json()
             rates = data['rates']
-            print(rates)
-            
+            #print(rates)
+
+            for currency, rate in rates.items():
+                moeda = ({f"{currency}: {rate:.4f}"})
+                print(", ".join(map(str, moeda)))
+        
         else:
-            print("Falha ao conectar na API")
+            if "error" in data:
+                print(f"Erro na API: {data['error']['info']}")
+                return
+        dataFrame(rates)
+            
+def dataFrame(rates):
+
+    df = pd.DataFrame(rates.items(), columns=["Moeda", "Taxa"])
+    df.to_csv("taxas_cambio.csv", index=False)
+    print("Dados salvos em taxas_cambio.csv\nCâmbio: Euro")
+    df = pd.read_csv("taxas_cambio.csv")
+    print(df)
 
 
 
